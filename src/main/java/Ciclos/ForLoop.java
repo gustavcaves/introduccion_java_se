d0fe11031d033e97e796a2448f05060d505c24e3
gustavcaves <rungustavo@gmail.com>
2022-06-27 17:47:54 +0200

    del while looo al for loop, reduciendo codigo

---
+++ src/main/java/Ciclos/ForLoop.java
@@ -0,0 +1,28 @@
+package Ciclos;
+
+public class ForLoop {
+
+    static boolean isTurnOnLight = false;
+
+    public static void main(String[] args) {
+        turnOnOffLight();
+
+        for (int i = 1; i <= 10; i++) {
+            printSOS();
+        }
+    }
+
+    public static void printSOS(){
+        System.out.println(". . . _ _ _ . . .");
+    }
+
+    public static boolean turnOnOffLight(){
+        isTurnOnLight = (isTurnOnLight)?false:true;
+//        if (isTurnOnLight){
+//            isTurnOnLight = false;
+//        } else {
+//            isTurnOnLight = true;
+//        }
+        return isTurnOnLight;
+    }
+}
