fc3ce7a935de47a1ffa6ed46d1102bfa2c2814b6
gustavcaves <rungustavo@gmail.com>
2022-06-27 18:18:34 +0200

    arrays, coleccion de datos, es un objeto

---
+++ src/main/java/Arreglos/Arrays.java
@@ -0,0 +1,15 @@
+package Arreglos;
+
+public class Arrays {
+    public static void main(String[] args) {
+        String[] androidVersions = new String[17];
+        String days[] = new String[7];
+
+        String[][] cities = new String[4][2]; // 4 * 2 = 8
+
+        // si es de 3 dimensiones llevar a bases de datos, se manejan mejor allí
+        int [][][] numbers = new int[2][2][2];
+        // se requieren ya para calculos mas avanzados, poco requeridos en programacion como tal
+        int [][][] numbers4 = new int[][][];
+    }
+}
